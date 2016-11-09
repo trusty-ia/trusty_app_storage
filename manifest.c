035da8739c620053267d27acac38fd09ed3110e9
Yan, Shaopu <shaopu.yan@intel.com>
2016-11-09 14:15:48 +0800

    [storage] Unify the definitions of uuid for TAs
    Change-Id: I6f641d57400d0f4494aaef6d770aadaeb9b1c345
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- manifest.c
+++ manifest.c
@@ -20,10 +20,9 @@
 
 trusty_app_manifest_t TRUSTY_APP_MANIFEST_ATTRS trusty_app_manifest =
 {
-    .uuid = { /* cea8706d-6cb4-49f3-b994-29e0e478bd29 */
-        0xcea8706d, 0x6cb4, 0x49f3,
-        { 0xb9, 0x94, 0x29, 0xe0, 0xe4, 0x78, 0xbd, 0x29 }
-    },
+    .uuid = SECURE_STORAGE_SERVER_APP_UUID,
+
+    .config_options =
     {
         TRUSTY_APP_CONFIG_MIN_STACK_SIZE(4 * 4096),
         TRUSTY_APP_CONFIG_MIN_HEAP_SIZE(8 * 4096 + BLOCK_CACHE_SIZE_BYTES),
