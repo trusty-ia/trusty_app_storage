cfeb5aa2f4d4d6cca289d2145864d4ff63359dc4
duminx <minx.du@intel.com>
2017-12-21 14:21:42 -0500

    update key migration in storage
    Change-Id: I46bd84c87afb569e196d82ae259866142e86d8ab
    Signed-off-by: duminx <minx.du@intel.com>

--- proxy.c
+++ proxy.c
@@ -22,12 +22,18 @@
 
 #include <interface/storage/storage.h>
 #include <lib/hwkey/hwkey.h>
+#include <trusty_key_migration.h>
 
 #include "ipc.h"
 #include "rpmb.h"
 #include "session.h"
 
 #define SS_ERR(args...)  fprintf(stderr, "ss: " args)
+#define CRYPTO_CONTEXT_RPMB_ADDR	(1024)
+
+static bool init_connection = true;
+static uint8_t g_rpmb_key[32] = {0};
+static bool g_setup_flag = false;
 
 static void proxy_disconnect(struct ipc_channel_context *ctx);
 
@@ -43,24 +49,11 @@ static struct storage_session *proxy_context_to_session(struct ipc_channel_conte
 static int get_storage_encryption_key(hwkey_session_t session, uint8_t *key,
                                       uint32_t key_size)
 {
-	static const struct key storage_key_derivation_data = {
-		.byte = {
-			0xbc, 0x10, 0x6c, 0x9e, 0xc1, 0xa4, 0x71, 0x04,
-			0x83, 0xab, 0x03, 0x4b, 0x75, 0x8a, 0xb3, 0x5e,
-			0xfb, 0xe5, 0x43, 0x6c, 0xe6, 0x74, 0xb7, 0xfc,
-			0xee, 0x20, 0xad, 0xae, 0xfb, 0x34, 0xab, 0xd3,
-		}
-	};
+	uint32_t size = key_size;
 
-	if (key_size != sizeof(storage_key_derivation_data.byte)) {
-		return ERR_BAD_LEN;
-	}
-
-	uint32_t kdf_version = HWKEY_KDF_VERSION_1;
-	int rc = hwkey_derive(session, &kdf_version, storage_key_derivation_data.byte,
-	                      key, key_size);
-	if (rc < 0) {
-		SS_ERR("%s: failed to get key: %d\n", __func__, rc);
+	int rc = hwkey_get_ssek(session, key, size);
+	if (rc) {
+		SS_ERR("%s: failed to get encryption key: %d, size: %u.\n", __func__, rc, size);
 		return rc;
 	}
 
@@ -72,6 +65,7 @@ static int get_rpmb_auth_key(hwkey_session_t session, uint8_t *key,
 {
 	const char *storage_auth_key_id =
 	        "com.android.trusty.storage_auth.rpmb";
+
 	int rc = hwkey_get_keyslot_data(session, storage_auth_key_id, key,
 	                                &key_size);
 	if (rc < 0) {
@@ -82,18 +76,69 @@ static int get_rpmb_auth_key(hwkey_session_t session, uint8_t *key,
 	return NO_ERROR;
 }
 
+/* if rpmb is not programmed, use seed[0] derived rpmb auth key to program.
+ * else, test up to BOOTLOADER_SEED_MAX_ENTRIES keys to get the correct one.
+ */
+static int get_programmed_rpmb_auth_key(handle_t chan_handle, hwkey_session_t hwkey_session)
+{
+	struct rpmb_key rpmb_keys[BOOTLOADER_SEED_MAX_ENTRIES];
+	struct rpmb_state state;
+	uint32_t i;
+	int rc = -1;
+	uint32_t write_counter = 0;
+	uint16_t result = -1;
+
+	/* Init RPMB key */
+	rc = get_rpmb_auth_key(hwkey_session, (uint8_t *)&rpmb_keys, sizeof(rpmb_keys));
+	if (rc < 0) {
+		SS_ERR("%s: can't get storage auth key: (%d)\n", __func__, rc);
+		goto out;
+	}
+
+	state.mmc_handle = &chan_handle;
+	for (i = 0; i < BOOTLOADER_SEED_MAX_ENTRIES; i++) {
+		memcpy_s(&state.key, 32, rpmb_keys[i].byte, 32);
+		rc = rpmb_read_counter(&state, &write_counter, &result);
+		if (rc == 0)
+			break;
+		if (result == RPMB_RES_NO_AUTH_KEY) {
+			SS_ERR("%s: key is not programmed.\n", __func__);
+			goto out;
+		}
+		if (result != RPMB_RES_AUTH_FAILURE) {
+			SS_ERR("%s: rpmb_read_counter unexpected error: %d.\n", __func__, result);
+			goto out;
+		}
+	}
+
+	if (i >= BOOTLOADER_SEED_MAX_ENTRIES) {
+		SS_ERR("%s: Fatal error: All keys are not match!\n", __func__);
+		goto out;
+	}
+
+	if (i != 0)
+		SS_ERR("%s: seed changed to %d.\n", __func__, i);
+
+	memcpy_s(g_rpmb_key, 32, rpmb_keys[i].byte, 32);
+	rc = 0;
+
+out:
+	secure_memzero(rpmb_keys, sizeof(rpmb_keys));
+	secure_memzero(&state, sizeof(struct rpmb_state));
+
+	return rc;
+}
+
 struct ipc_channel_context *proxy_connect(struct ipc_port_context *parent_ctx,
                                           const uuid_t *peer_uuid, handle_t chan_handle)
 {
 	struct rpmb_key rpmb_key;
-	struct rpmb_key rpmb_keys[BOOTLOADER_SEED_MAX_ENTRIES];
 	struct rpmb_state state;
-	uint32_t i;
-	int ret;
-	uint32_t write_counter = 0;
-	uint16_t result = -1;
-
 	int rc;
+	uint8_t buf[256] = {0};
+	struct crypto_context crypto_ctx = {0};
+	struct crypto_context updated_crypto_ctx = {0};
+	uint32_t i;
 
 	struct storage_session *session = calloc(1, sizeof(*session));
 	if (session == NULL) {
@@ -101,6 +146,7 @@ struct ipc_channel_context *proxy_connect(struct ipc_port_context *parent_ctx,
 		goto err_alloc_session;
 	}
 
+	memset(&state, 0, sizeof(struct rpmb_state));
 	session->magic = STORAGE_SESSION_MAGIC;
 
 	rc = hwkey_open();
@@ -111,45 +157,84 @@ struct ipc_channel_context *proxy_connect(struct ipc_port_context *parent_ctx,
 
 	hwkey_session_t hwkey_session = (hwkey_session_t) rc;
 
+	state.mmc_handle = &chan_handle;
+
+	SS_ERR("init_connection is %d.\n", init_connection);
+
+	if (init_connection) {
+		init_connection = false;
+
+		if (get_programmed_rpmb_auth_key(chan_handle, hwkey_session)) {
+			SS_ERR("%s: get_programmed_rpmb_auth_key failed.\n", __func__);
+			goto err_init_connection;
+		}
+
+		memcpy_s(state.key.byte, 32, g_rpmb_key, 32);
+		if (rpmb_read(&state, buf, CRYPTO_CONTEXT_RPMB_ADDR, 1)) {
+			SS_ERR("%s: rpmb_read CRYPTO_CONTEXT_RPMB_ADDR failed.\n", __func__);
+			goto err_init_connection;
+		}
+		if (memcpy_s(&crypto_ctx, sizeof(crypto_ctx), buf, sizeof(buf))) {
+			SS_ERR("%s: failed to copy buf to crypto_ctx.\n", __func__);
+			goto err_init_connection;
+		}
+
+		if (crypto_ctx.magic != CRYPTO_CONTEXT_MAGIC_DATA) {
+			SS_ERR("%s: CRYPTO CONTEXT ARE NOT EXISTED.\n", __func__);
+			if (hwkey_generate_crypto_context(hwkey_session, (uint8_t *)&crypto_ctx, sizeof(crypto_ctx))) {
+				SS_ERR("%s: hwkey_generate_crypto_context failed.\n", __func__);
+				goto err_init_connection;
+			}
+
+			if (memcpy_s(buf, sizeof(buf), &crypto_ctx, sizeof(crypto_ctx))) {
+				SS_ERR("%s: failed to copy crypto_ctx to buf.\n", __func__);
+				goto err_init_connection;
+			}
+
+			if (rpmb_write(&state, buf, CRYPTO_CONTEXT_RPMB_ADDR, 1, true)) {
+				SS_ERR("%s: rpmb_write CRYPTO_CONTEXT_RPMB_ADDR failed.\n", __func__);
+				goto err_init_connection;
+			}
+		}
+		else {
+			SS_ERR("%s: CRYPTO CONTEXT ARE EXISTED.\n", __func__);
+			if (hwkey_exchange_crypto_context(hwkey_session, (const uint8_t *)&crypto_ctx,
+						 (uint8_t *)&updated_crypto_ctx, sizeof(struct crypto_context))) {
+				SS_ERR("%s: hwkey_transfer_enc_seeds failed.\n", __func__);
+				goto err_init_connection;
+			}
+			if (memcmp(&crypto_ctx, &updated_crypto_ctx, sizeof(struct crypto_context))) {
+				SS_ERR("%s: SEED CHANGED!!! Rewrite to rpmb.\n", __func__);
+				if (memcpy_s(buf, sizeof(buf), &updated_crypto_ctx, sizeof(struct crypto_context))) {
+					SS_ERR("%s: failed to copy updated_crypto_ctx to buf.\n", __func__);
+					goto err_init_connection;
+				}
+				if (rpmb_write(&state, buf, CRYPTO_CONTEXT_RPMB_ADDR, 1, true)) {
+					SS_ERR("%s: rpmb_write CRYPTO_CONTEXT_RPMB_ADDR failed.\n", __func__);
+					goto err_init_connection;
+				}
+			}
+		}
+
+		g_setup_flag = true;
+		goto err_init_connection;
+	}
+
+	if (!g_setup_flag) {
+		SS_ERR("%s: init connection gets failure. Drop to connect.\n", __func__);
+		goto err_init_block_device;
+	}
+
 	/* Generate encryption key */
-	rc = get_storage_encryption_key(hwkey_session, session->key.byte,
-	                                sizeof(session->key));
+	rc = get_storage_encryption_key(hwkey_session, session->key.byte, sizeof(session->key.byte));
 	if (rc < 0) {
 		SS_ERR("%s: can't get storage key: (%d) \n", __func__, rc);
 		goto err_get_storage_key;
 	}
 
-	/* Init RPMB key */
-	rc = get_rpmb_auth_key(hwkey_session, &rpmb_keys, sizeof(rpmb_keys));
-	if (rc < 0) {
-		SS_ERR("%s: can't get storage auth key: (%d)\n", __func__, rc);
-		goto err_get_rpmb_key;
-	}
-	state.mmc_handle = &chan_handle;
-	for (i = 0; i < BOOTLOADER_SEED_MAX_ENTRIES; i++) {
-		memcpy_s(&state.key, 32, rpmb_keys[i].byte, 32);
-		ret = rpmb_read_counter(&state, &write_counter, &result);
-		if (ret == 0)
-			break;
-		if (result == RPMB_RES_NO_AUTH_KEY) {
-			SS_ERR("%s: key is not programmed, use the first seed to derive keys.\n", __func__);
-			break;
-		}
-		if (result != RPMB_RES_AUTH_FAILURE) {
-			SS_ERR("%s: rpmb_read_counter unexpected error: %d.\n", __func__, result);
-			goto err_get_rpmb_key;
-		}
-	}
-
-	if (i >= BOOTLOADER_SEED_MAX_ENTRIES) {
-		SS_ERR("%s: Fatal error: All keys are not match!\n", __func__);
-		goto err_get_rpmb_key;
-	}
-
-	if (i != 0)
-		SS_ERR("%s: seed changed to %d.\n", __func__, i);
-
-	memcpy_s(rpmb_key.byte, 32, rpmb_keys[i].byte, 32);
+	memcpy_s(&rpmb_key, 32, g_rpmb_key, 32);
+	/* keep in memory, because it may be called more than 2 times
+         * secure_memzero(g_rpmb_key, 32); */
 
 	rc = block_device_tipc_init(&session->block_device, chan_handle,
 	                            &session->key, &rpmb_key);
@@ -158,18 +243,21 @@ struct ipc_channel_context *proxy_connect(struct ipc_port_context *parent_ctx,
 		goto err_init_block_device;
 	}
 
-	memset(rpmb_keys, 0, sizeof(rpmb_keys));
 	session->proxy_ctx.ops.on_disconnect = proxy_disconnect;
 
 	hwkey_close(hwkey_session);
 
 	return &session->proxy_ctx;
 
+err_init_connection:
+	secure_memzero(&updated_crypto_ctx, sizeof(struct crypto_context));
+	secure_memzero(&crypto_ctx, sizeof(struct crypto_context));
+	secure_memzero(buf, sizeof(buf));
+	secure_memzero(&state, sizeof(state));
 err_init_block_device:
-	memset(&rpmb_key, 0, sizeof(rpmb_key));
-	memset(session->key.byte, 0, sizeof(session->key.byte));
 err_get_rpmb_key:
-	memset(rpmb_keys, 0, sizeof(rpmb_keys));
+err_program_key:
+	secure_memzero(session->key.byte, sizeof(session->key.byte));
 err_get_storage_key:
 	hwkey_close(hwkey_session);
 err_hwkey_open:
@@ -186,4 +274,3 @@ void proxy_disconnect(struct ipc_channel_context *ctx)
 
 	free(session);
 }
-
